be7f4d38580e38104fa1499e026cdfc35a9e08bf
abibi189 <145033589+abibi189@users.noreply.github.com>
2025-01-24 19:59:17 +0000

    add functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,30 @@
-function hello(){
-    return "hello";
+// Test Example in Lecture
+function celsius2Fahrenheit(celsius){
+    let fahrenheit = 9/5 * celsius + 32;
+    return(fahrenheit)
 }
+console.log(`The boiling point of water in fahrenheit is ${celsius2Fahrenheit(100)}`)
 
+// 1) Convert meters to yards
+function meters2Yards(meters){
+    let yards = meters * 1.0;
+    return(yards)
+}
+console.log(`The distance in yards is ${meters2Yards(20)}`)
 
-export {hello}
+// 2) Clothing suggestion based by temperature
+function clothingSuggestion(temp){
+    if (temp > 25) {
+        return "light clothes";
+    } else {
+        return "a warm coat";
+    }
+}
+console.log(`For a temperature of 28°C, you should wear ${clothingSuggestion(28)}`)
+
+// 3) Calculate the volume of a cube
+function cubeVolume(edge){
+    let volume = edge * edge * edge;
+    return volume;
+}
+console.log(`The volume of a cube with an edge length of 2 meters is ${cubeVolume(2)} cubic meters`)
